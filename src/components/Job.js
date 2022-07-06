8f58ed69f7fccc4118f7cafda50346e82b465c06
ackerm44 <ackerm44@gmail.com>
2022-07-06 12:33:05 -0400

    Job display updates

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,9 +1,14 @@
 
 const Job = ({title, company, date, source, link}) => {
   return (
-    <div>
-        <p>{title} - {company}</p>
-    </div>
+    <tr>
+        <td>{date}</td>
+        <td>{title}</td>
+        <td>{company}</td>
+        <td>{source}</td>
+        <td><a href={link} target="_blank" rel="noreferrer">{`${link.slice(0,50)}...`}</a></td>
+
+    </tr>
   )
 }
 
--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,18 +1,33 @@
 import Job from "./Job"
 
-const Jobs = ({jobs}) => {
-  return (
-    <section>
-        {
-            jobs.map((job) => {
-                return (
-                   <Job key={job.id} {...job}/>
-                )
-            })
+const Jobs = ({ jobs }) => {
+    return (
+        <section className="list">
+            <table className="table-grid">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Title</th>
+                        <th>Company</th>
+                        <th>Source</th>
+                        <th>Link</th>
+                    </tr>
 
-        }
-    </section>
-  )
+                </thead>
+                <tbody>
+                    {
+                        jobs.map((job) => {
+                            return (
+                                <Job key={job.id} {...job} />
+                            )
+                        })
+
+                    }
+                </tbody>
+            </table>
+
+        </section>
+    )
 }
 
 export default Jobs
