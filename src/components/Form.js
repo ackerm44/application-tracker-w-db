73fb2703b508eacdacc0ca10b04993d324494354
ackerm44 <ackerm44@gmail.com>
2022-07-07 14:34:03 -0400

    Add more data fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,42 +1,86 @@
-import { useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 
-const Form = ({ onAdd }) => {
+const Form = ({ onAdd, onEdit, editID }) => {
     const [title, setTitle] = useState("")
     const [company, setCompany] = useState("")
-    const [date, setDate] = useState("")
+    const [dateOrigination, setDateOrigination] = useState("")
+    const [dateApplied, setDateApplied] = useState("")
     const [source, setSource] = useState("")
     const [link, setLink] = useState("")
+    const [status, setStatus] = useState("")
     const [sources, setSources] = useState([])
+    const [notes, setNotes] = useState("")
+    const [statuses, setStatuses] = useState([])
 
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        onAdd({ title, company, date, source, link })
+        if (editID) {
+            let id = editID
+            onEdit({id, title, company, dateOrigination, dateApplied, source, link, status, notes })
+        } else {
+            onAdd({ title, company, dateOrigination, dateApplied, source, link, status, notes })
+        }
+        
         setTitle("")
         setCompany("")
-        setDate("")
+        setDateOrigination("")
+        setDateApplied("")
         setSource(sources[0].title)
         setLink("")
+        setStatus(statuses[0].title)
     }
 
-      const fetchSources = async () => {
+    const fetchSources = async () => {
         const res = await fetch('http://localhost:5000/sources')
         const data = await res.json()
         setSources(data)
         setSource(data[0].title)
     }
 
+    const fetchStatuses = async () => {
+        const res = await fetch('http://localhost:5000/statuses')
+        const data = await res.json()
+        setStatuses(data)
+        setStatus(data[0].title)
+    }
+
+    const fetchJob = async (editID) => {
+        const res = await fetch(`http://localhost:5000/jobs/${editID}`)
+        const data = await res.json()
+
+        setTitle(data.title)
+        setCompany(data.company)
+        setDateOrigination(data.dateOrigination)
+        setDateApplied(data.dateApplied)
+        setSource(data.source)
+        setLink(data.link)
+        setStatus(data.status)
+    }
+
     useEffect(() => {
         fetchSources()
+        fetchStatuses()
+
     }, [])
+
+    useEffect(() => {
+        if (editID) {
+            fetchJob(editID)
+        }
+    }, [editID])
 
 
     return (
         <section>
             <form onSubmit={handleSubmit}>
                 <div className="form-control">
+                    <label htmlFor="dateOrigination">Date of Origination</label>
+                    <input type="date" name="dateOrigination" value={dateOrigination} onChange={e => setDateOrigination(e.target.value)}></input>
+                </div>
+                <div className="form-control">
                     <label htmlFor="date">Date Applied</label>
-                    <input type="date" name="date" value={date} onChange={e => setDate(e.target.value)}></input>
+                    <input type="date" name="dateApplied" value={dateApplied} onChange={e => setDateApplied(e.target.value)}></input>
                 </div>
                 <div className="form-control">
                     <label htmlFor="title">Title</label>
@@ -48,16 +92,27 @@ const Form = ({ onAdd }) => {
                 </div>
                 <div className="form-control">
                     <label htmlFor="source">Source</label>
-                    <select name="source" onChange={e => setSource(e.target.value)}>
-                        {sources.map((source) => 
+                    <select name="source" value={source.title} onChange={e => setSource(e.target.value)}>
+                        {sources.map((source) =>
                             <option key={source.id} value={source.title}>{source.title}</option>
                         )}
                     </select>
-                    {/* <input type="text" name="source" value={source} onChange={e => setSource(e.target.value)}></input> */}
+                </div>
+                <div className="form-control">
+                    <label htmlFor="status">Status</label>
+                    <select name="status" onChange={e => setStatus(e.target.value)}>
+                        {statuses.map((status) =>
+                            <option key={status.id} value={status.title}>{status.title}</option>
+                        )}
+                    </select>
                 </div>
                 <div className="form-control">
                     <label htmlFor="link">Link</label>
                     <input type="url" name="link" value={link} onChange={e => setLink(e.target.value)}></input>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="notes">Notes</label>
+                    <textarea name="notes" rows="4" cols="50" value={notes} onChange={e => setNotes(e.target.value)}></textarea>
                 </div>
                 <button className="submit">Submit</button>
             </form>
